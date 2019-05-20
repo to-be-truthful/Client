a96c73922990b3df4c767e4029dea1b76d298944
AlecDusheck <a.dusheckirl@gmail.com>
2019-05-19 23:18:34 -0500

    Change profile images

--- src/app/components/home/home.page.ts
+++ src/app/components/home/home.page.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {APIService, INotif, IRate} from "../../providers/api.service";
+import {ToastController} from "@ionic/angular";
 
 @Component({
     selector: 'app-home',
@@ -7,10 +9,72 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-    constructor() {
+    public rates: Array<IRate>;
+
+    constructor(
+        private apiService: APIService,
+        private toastController: ToastController
+    ) {
     }
 
     ngOnInit() {
+        this.apiService.getFeed().then(feed => {
+            console.log(feed);
+            this.rates = feed.rates;
+
+            feed.notifs.forEach(notif => {
+                this.createNotif(notif.text);
+            })
+        })
     }
 
+    public createNotif = async (message: string) => {
+        const toast = await this.toastController.create({
+            header: "Notification",
+            message: message,
+            buttons: [
+                {
+                    text: 'Close',
+                    role: 'cancel',
+                }
+            ]
+        } );
+        toast.present();
+    };
+
+    public loadContent = () => {
+        this.apiService.getFeed().then(feed => {
+            console.log(feed);
+            this.rates = feed.rates;
+
+            feed.notifs.forEach(notif => {
+                this.createNotif(notif.text);
+            });
+        })
+    };
+
+    public getTimeSince = (dateString: string): string => {
+        const diffTime = Math.abs(new Date(dateString).getTime() - Date.now());
+        return this.msToTime(diffTime);
+    };
+
+    private msToTime = (duration: number) => {
+        const seconds = Math.floor((duration / 1000) % 60);
+        const minutes = Math.floor((duration / (1000 * 60)) % 60);
+        const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+        let hourFormat = "hour";
+        let minuteFormat = "minute";
+        let secondFormat = "second";
+
+        if (hours !== 1)hourFormat += "s";
+        if (minutes !== 1) minuteFormat += "s";
+        if (seconds !== 1) secondFormat += "s";
+
+        if (hours > 0){
+            return hours + " " + hourFormat + " and " + minutes + " " + minuteFormat;
+        } else {
+            return minutes + " " + minuteFormat + " and " + seconds + " " + secondFormat;
+        }
+    };
 }
