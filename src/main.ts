ddcd34397ec231d99c80c243afa972fd5aea56bb
cmptrcharger <cmptrcharger@gmail.com>
2019-05-28 09:48:20 -0500

    Added a comment somewhere in one of the files, good luck and have fun

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import {environment} from './environments/environment';
 
 console.log("To Be Truthful is a final for the AP Computer Science course. Made by Alec Dusheck and Sam Martin (wut?)");
 console.log("Good luck debugging lmao! (ur gonna need it)");
+console.log("I added an easter egg, find it.");
 
 if (environment.production) {
     enableProdMode();
